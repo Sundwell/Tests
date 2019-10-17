4b4c59f773eb5b99761abdedc2ec94dfb75fca8d
Serhii <lyamtsev.s2017@gmail.com>
2019-10-17 17:04:31 +0300

    deleted directory venv, updated log_decorator

--- Exercises_17-10-2019/log_decorator.py
+++ Exercises_17-10-2019/log_decorator.py
@@ -3,20 +3,18 @@ my_dict = {}
 
 def decorator(func):
     def wrapper(name, points):
-        print(f'args are: {name}, {points}')
-        result = func(name, points)
-        return result
+        my_dict.update(func(name, points))
+        return my_dict
     return wrapper
 
 
 @decorator
 def print_some(name, points):
-    my_dict.update({name: points})
-    return my_dict
+    return {name: points % 100}
 
 
 if __name__ == "__main__":
-    print_some("Soren", 100)
+    print_some("Soren", 210)
     print_some("Andrew", 53)
     print_some("Sarah", 76)
     print_some("Maxwell", 58)
