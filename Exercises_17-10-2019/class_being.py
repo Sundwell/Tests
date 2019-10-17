8e8c225d9b90586d21e2b3ab13e9ff6ea75534a0
Serhii <lyamtsev.s2017@gmail.com>
2019-10-17 12:50:42 +0300

    added a class_being

---
+++ Exercises_17-10-2019/class_being.py
@@ -0,0 +1,40 @@
+class Mixin(object):
+    def __init__(self):
+        self.name = object.name
+
+    def move(self):
+        print(f'{self.name} is moving')
+
+    def close_eyes(self):
+        print(f'{self.name} has closed eyes')
+
+
+class Being:
+    def __init__(self, name):
+        self.name = name
+
+
+class Cat(Being, Mixin):
+    def __init__(self, name):
+        super(Cat, self).__init__(name)
+
+
+class Dog(Being, Mixin):
+    def __init__(self, name):
+        super(Dog, self).__init__(name)
+
+
+class Bird(Being, Mixin):
+    def __init__(self, name):
+        super(Bird, self).__init__(name)
+
+
+cat = Cat('Jake')
+cat.move()
+cat.close_eyes()
+
+dog = Dog('Doggy')
+dog.move()
+dog.close_eyes()
+
+
