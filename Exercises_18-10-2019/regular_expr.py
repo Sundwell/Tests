5288214266f0e756e7c230cb4de1be0de85e32cd
Serhii <lyamtsev.s2017@gmail.com>
2019-10-18 16:36:24 +0300

    updated regular_expr (deleted numbers)

--- Exercises_18-10-2019/regular_expr.py
+++ Exercises_18-10-2019/regular_expr.py
@@ -2,7 +2,7 @@ import re
 
 # Рейзит тайп эрор если в строке есть буквы не из укр алфавита
 user_str = input('Enter some ukrainian str:')
-pattern = re.compile(r'[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ`1-9, .]+')
+pattern = re.compile(r'[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ`, .]+')
 result = ''.join(pattern.findall(user_str))
 if result != user_str:
     raise TypeError
@@ -10,7 +10,7 @@ if result != user_str:
 
 # Рейзит тайп эрор если в строке есть буквы не из англ алфавита
 # user_str = input('Enter english str:')
-# pattern = re.compile(r'[a-zA-Z1-9, \.]+')
+# pattern = re.compile(r'[a-zA-Z, .]+')
 # result = ''.join(pattern.findall(user_str))
 # if result != user_str:
 #     raise TypeError
