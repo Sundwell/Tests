b5ed78ceb3fc0bcf2d5b181165e012fa655c5afe
Serhii <lyamtsev.s2017@gmail.com>
2019-10-18 18:23:02 +0300

    added product_class

--- Exercises_18-10-2019/product.py
+++ Exercises_18-10-2019/product.py
@@ -15,11 +15,11 @@ class Product:
     prod_name = 'NoName'
 
     def __init__(self, prod_name, count, date_of_expire):
-        self.set_prod_name(prod_name)
+        self.__set_prod_name(prod_name)
         self.date_of_expire = date_of_expire
         self.id = Product.id[-1]
         Product.id.append(Product.id[-1] + 1)
-        self.set_count(count)
+        self.__set_count(count)
 
     def check_condition(self):
         if self.date_of_expire >= datetime.datetime.today().strftime("%d-%m-%Y"):
@@ -29,13 +29,13 @@ class Product:
             self.is_spoiled = False
             print(f'{self.prod_name} is in good condition')
 
-    def set_prod_name(self, prod_name):
+    def __set_prod_name(self, prod_name):
         if prod_name:
             self.prod_name = prod_name
         else:
             raise NameError("Wrong name of product")
 
-    def set_count(self, count):
+    def __set_count(self, count):
         if count >= 0:
             self.count = count
         else:
